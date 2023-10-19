ad7afae4e1347552ec6255adf14233b166c86efc
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2023-10-20 04:10:21 +0600

    post category, get category list

--- index.js
+++ index.js
@@ -30,14 +30,47 @@ async function run() {
         await client.connect();
 
         const automotiveCollection = client.db('automotiveDB').collection('automotive');
+        const categoryCollection = client.db("automotiveDB").collection("category");
+        // for show product
+        app.get('/products', async (req, res) => {
 
+            const cursor = automotiveCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // for show category data
+        app.get('/categoryList', async (req, res) => {
+            const cursor = categoryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+
+        })
+
+
+        app.get('/productLists/:brandName', async (req, res) => {
+            const brandName = res.params.brandName;
+            const cursor = automotiveCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+
+        })
+
+
+        // create product 
         app.post('/products', async (req, res) => {
             const newProduct = req.body;
             console.log(newProduct);
             const result = await automotiveCollection.insertOne(newProduct)
             res.send(result);
         })
-
+        // create category
+        app.post('/category', async (req, res) => {
+            const newCategory = req.body;
+            console.log(newCategory);
+            const result = await categoryCollection.insertOne(newCategory);
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
